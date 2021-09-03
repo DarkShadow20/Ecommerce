a3c355d6856e3a6c5eff0fb83e61bdded078bcbd
DarkShadow20 <kunal8011@gmail.com>
2021-09-03 10:46:17 +0530

    feat:guest user login

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -14,32 +14,41 @@ export const Login = () => {
   const [error, setError] = useState("");
   const [togglePassword, setTogglePassword] = useState(true);
 
-  const loginBtnHandler = () => {
-    if (validateUserInput({ email }).checkEmail) {
-      setError("");
-      axios
-        .post("https://Ecom.kunalgupta9.repl.co/users/login", {
-          email,
-          password
-        })
-        .then((res) => {
-          if (!res.data.success) {
-            setError("Email or password didn't match!");
-          }
-          setLogin(res.data.success);
-          if (res.data.success) {
-            setUserData(res.data.user)
-            navigate(location?.state?.from ? location.state.from : "/");
-          }
-          setUserName(res.data.name);
-        });
-    } else {
+  const guestUserHandler=(e)=>{
+    setEmail("ram@gmail.com")
+    setPassword("Lolo@123")
+  }
+  const loginBtnHandler = async (e) => {
+    e.preventDefault();
+    try{
+      if (validateUserInput({ email }).checkEmail) {
+        setError("");
+        axios
+          .post("https://Ecom.kunalgupta9.repl.co/users/login", {
+            email,
+            password
+          })
+          .then((res) => {
+            if (!res.data.success) {
+              setError("Email or password didn't match!");
+            }
+            setLogin(res.data.success);
+            if (res.data.success) {
+              setUserData(res.data.user)
+              navigate(location?.state?.from ? location.state.from : "/");
+            }
+            setUserName(res.data.name);
+          });
+      }
+    }
+     catch(err) {
       setError("Enter valid email!");
     }
   };
 
   return (
     <>
+    <form onSubmit={loginBtnHandler}>
     <div className="login-wrapper">
       {isUserLoggedIn && (
         <Navigate to={location?.state?.from ? location.state.from : "/"} />
@@ -89,8 +98,11 @@ export const Login = () => {
           Sign up
         </Link>
       </button>
-      <button onClick={loginBtnHandler}>Login</button>
+      <button onClick={loginBtnHandler} style={{cursor:"pointer"}}>Login</button>
+      <br/>
+      <button style={{marginLeft:"1rem",width:"10rem", cursor:"pointer"}} onClick={guestUserHandler}>Login as GuestUser</button>
     </div>
+    </form>
     </>
   );
 };
